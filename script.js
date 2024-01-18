d5b9284d58b3b73352553fcba222088aa7c6f95a
Pravin Kumar <143949225+mavinCoder@users.noreply.github.com>
2024-01-18 23:06:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,3 @@ function closePopup(){
     document.getElementById("contactPopup").style.visibility="hidden";
     document.getElementById("screen").style.filter="blur(0px)";
 }
-
-function sentToastNotification() {
-    var Notification = document.getElementById('MessageSent');
-    Notification.style.visibility = "visible";
-    setTimeout(function () {
-        Notification.style.visibility = "hidden";
-    }, 2500);
-}
